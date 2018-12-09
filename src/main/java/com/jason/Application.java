06f7e45f811df62cd06239116385eab895a1cdf6
lpli <leep123@qq.com>
2018-12-09 21:20:56 +0800

    druid配置

--- src/main/java/com/jason/Application.java
+++ src/main/java/com/jason/Application.java
@@ -2,18 +2,20 @@ package com.jason;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Profile;
 
 /**
  * @Author liliping
  * @Date 2018/11/20
  **/
-@SpringBootApplication
-@MapperScan("com.")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(Application.class, args);
     }
 }
